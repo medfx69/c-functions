5136209b350ec82353c48103e41920a23b85f665
Mohamed Ait aadi <mait-aad@e1r10p5.1337.ma>
2021-11-16 23:48:02 +0100

    544846

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 01:37:56 by mait-aad          #+#    #+#             */
-/*   Updated: 2021/11/16 22:19:57 by mait-aad         ###   ########.fr       */
+/*   Updated: 2021/11/16 23:45:31 by mait-aad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,9 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list	*lst, void (*del)(void*));
 
 #endif
