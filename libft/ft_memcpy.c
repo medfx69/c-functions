0f83c9caef92464aa2cf1069dfc990e0adb92cd7
Mohamed Ait aadi <mait-aad@e1r3p6.1337.ma>
2021-11-19 21:19:48 +0100

    5646

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/05 11:36:46 by mait-aad          #+#    #+#             */
-/*   Updated: 2021/11/19 19:27:42 by mait-aad         ###   ########.fr       */
+/*   Updated: 2021/11/19 21:18:33 by mait-aad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memcpy(void	*dest, void const	*src, size_t	n)
 	s2 = (unsigned char *) src;
 	if (dest || src)
 	{
-		while (n-- > 0 && *s2)
+		while (n--)
 		{
 			*s1++ = *s2++;
 		}
