a3409ba1deb7ff28d392e5a061a8e6589d4c63ba
Mohamed Ait aadi <mait-aad@e1r12p1.1337.ma>
2021-11-20 15:57:21 +0100

    9450

---
+++ bonus/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mait-aad <mait-aad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 17:11:26 by mait-aad          #+#    #+#             */
+/*   Updated: 2021/11/16 21:55:30 by mait-aad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*str;
+
+	str = (t_list *)malloc(sizeof(t_list));
+	if (!str)
+		return (NULL);
+	str->content = content;
+	str->next = NULL;
+	return (str);
+}
